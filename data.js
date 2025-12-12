b0064db773fea4b58ec427642a176435b19b1f01
chooocat <120656085+chooocat@users.noreply.github.com>
2025-12-12 15:59:22 +0800

    v0.10

--- data.js
+++ data.js
@@ -1,109 +1,284 @@
 function E(x) {return new Decimal(x)}
 
-var player = {
-    points: E(1),
-    softcapLevel: 0,
+function getRawData() {
+    return {
+        didHardReset: false,
+
+        points: {
+            amount: E(1),
+            best: E(1),
+            level: E(0),
+            levelXp: E(0),
+        },
+        prestige: {
+            points: E(0),
+            best: E(0),
+            total: E(0),
+            level: E(0),
+            levelXp: E(0),
+            prestiged: false,
+        },
+
+        unlocks: {
+            prestige: false,
+        },
+
+        //doCost types:
+        //0 - none
+        //1 - normal (subtract)
+        //2 - reset (with resetTo option)
+        upgrades: {
+            points: {
+                1: {
+                    level: E(0),
+                    levelCap: E('10^^100'),
+                    baseCost: E(1e4),
+                    cost: E(1e4),
+                    doCost: 2,
+                    currency: 0,
+                    resetTo: 1,
+                },
+                2: {
+                    level: E(0),
+                    levelCap: E('30'),
+                    baseCost: E(3e6),
+                    cost: E(3e6),
+                    doCost: 2,
+                    currency: 0,
+                    resetTo: 1,
+                },
+                3: {
+                    level: E(0),
+                    levelCap: E('10^^100'),
+                    baseCost: E('1e100'),
+                    cost: E('1e100'),
+                    doCost: 2,
+                    currency: 0,
+                    resetTo: 1,
+                },
+            },
+            prestige: {
+                1: {
+                    level: E(0),
+                    levelCap: E('10^^100'),
+                    baseCost: E(3),
+                    cost: E(3),
+                    doCost: 1,
+                    currency: 1,
+                    resetTo: 1,
+                },
+                2: {
+                    level: E(0),
+                    levelCap: E('10^^100'),
+                    baseCost: E(5e3),
+                    cost: E(5e3),
+                    doCost: 1,
+                    currency: 1,
+                    resetTo: 1,
+                },
+            },
+        },
+
+        options: {
+            confirms: {
+                prestige: true,
+            }
+        },
+    };
 }
 
-//doCost types:
-//0 - none, meaning the currency will get unaffected
-//1 - subtraction (currency -= cost)
-//2 - reseting (req. to fill "resetTo" property)
-var upgrades = {
-    points: {
-        1: {
-            level: E(0),
-            levelCap: E('10^^100'),
-            baseCost: E(1e4),
-            cost: E(1e4),
-            doCost: 2,
-            resetTo: E(1), //only for doCost 2's
-        },
-        2: {
-            level: E(0),
-            levelCap: E('30'),
-            baseCost: E(3e6),
-            cost: E(3e6),
-            doCost: 2,
-            resetTo: E(1), //only for doCost 2's
-        },
-    }
+var player = getRawData();
+
+var nonSavePlayer = {
+    softcapLevel: 0,
+    prestigePointGain: E(0),
+    pointXpReq: E(0),
+    pointXpCur: E(0),
+    prestigeXpReq: E(0),
+    prestigeXpCur: E(0),
 }
 
 var upgradeFormulas = {
     points: {
         1: {
-            costForm: function(remLevel = E(0)) {let upgrade = upgrades.points['1']; return upgrade.baseCost.mul(E(25).pow(upgrade.level.sub(remLevel)))},
-            levelForm: function() {let upgrade = upgrades.points['1']; return player.points.div(upgrade.baseCost).log(25).add(1).floor()}
+            costForm: function(remLevel = E(0)) {
+                let upgrade = player.upgrades.points['1']
+                if (upgrade.level.gte(70)) {
+                    return upgrade.baseCost.mul(E(40).pow(upgrade.level.sub(remLevel)))
+                }
+                return upgrade.baseCost.mul(E(25).pow(upgrade.level.sub(remLevel)))
+            },
+            levelForm: function() {
+                let upgrade = player.upgrades.points['1']
+                if (upgrade.level.gte(70)) {
+                    return player.points.amount.div(upgrade.baseCost).log(40).add(1).floor()
+                }
+                return player.points.amount.div(upgrade.baseCost).log(25).add(1).floor()
+            }
         },
         2: {
-            costForm: function(remLevel = E(0)) {let upgrade = upgrades.points['2']; return upgrade.baseCost.mul(E(200).pow(upgrade.level.sub(remLevel)))},
-            levelForm: function() {let upgrade = upgrades.points['2']; return player.points.div(upgrade.baseCost).log(200).add(1).floor()}
+            costForm: function(remLevel = E(0)) {let upgrade = player.upgrades.points['2']; return upgrade.baseCost.mul(E(200).pow(upgrade.level.sub(remLevel)))},
+            levelForm: function() {let upgrade = player.upgrades.points['2']; return player.points.amount.div(upgrade.baseCost).log(200).add(1).floor()}
         },
-    }
+        3: {
+            costForm: function(remLevel = E(0)) {let upgrade = player.upgrades.points['3']; return upgrade.baseCost.mul(E('1e10').pow(upgrade.level.sub(remLevel)))},
+            levelForm: function() {let upgrade = player.upgrades.points['3']; return player.points.amount.div(upgrade.baseCost).log('1e10').add(1).floor()}
+        },
+    },
+    prestige: {
+        1: {
+            costForm: function(remLevel = E(0)) {let upgrade = player.upgrades.prestige['1']; return upgrade.baseCost.mul(E(2).pow(upgrade.level.sub(remLevel)))},
+            levelForm: function() {let upgrade = player.upgrades.prestige['1']; return player.prestige.points.div(upgrade.baseCost).log(2).add(1).floor()}
+        },
+        2: {
+            costForm: function(remLevel = E(0)) {let upgrade = player.upgrades.prestige['2']; return upgrade.baseCost.mul(E(3).pow(upgrade.level.sub(remLevel)))},
+            levelForm: function() {let upgrade = player.upgrades.prestige['2']; return player.prestige.points.div(upgrade.baseCost).log(3).add(1).floor()}
+        },
+    },
 }
+
+var currencyStringValues = [
+    "points.amount",
+    "prestige.points",
+]
 
 var multi = {
     points: {
         add_base: E(2),
         mul_upg_p1: E(1),
+        mul_upg_pr1: E(1),
+        mul_prestigeBonus: E(1),
+        mul_pointLevelBonus: E(1),
         div_softcap1: E(1),
         div_softcap2: E(1),
-        root_softcap3: E(1),
+        div_softcap3: E(1),
+        div_softcap4: E(1),
+
+        xp: {
+            add_base: E(0),
+            mul_prestigeLevelBonus: E(1),
+            mul_upg_p3: E(1),
+            mul_upg_pr2: E(1),
+        },
 
         softcap_start: {
             1: {
                 add_base: E(100),
                 mul_upg_p2: E(1),
-            }
+                mul_pointLevelBonus: E(1),
+            },
+            2: {
+                add_base: E('1e40'),
+                mul_pointLevelBonus: E(1),
+            },
         },
-    }
+    },
+    prestige: {
+        add_base: E(1),
+        mul_prestigeLevelBonus: E(1),
+
+        xp: {
+            add_base: E(0)
+        },
+    },
+
+    other: {}
 }
 
-var clearPlayer = player
-var clearUpgrades = upgrades
-
-function convertObjectValuesToEtern(obj) {
-    const newObj = {};
-
-    for (const key in obj) {
-        if (Object.prototype.hasOwnProperty.call(obj, key)) {
-            const value = obj[key];
-            if (typeof value === 'string') {
-                newObj[key] = E(value); // Convert to break_eternity
-            } else if (typeof value === 'object') {
-                newObj[key] = convertObjectValuesToEtern(value)
-            } else {
-                newObj[key] = value; // Keep other types as they are
-            }
-        }
-    }
-    return newObj;
-}
+//for saving and loading i used the TMT code cuz i cant
 
 function save() {
-    localStorage.setItem("game-data_player", JSON.stringify(player))
-    localStorage.setItem("game-data_upgrades", JSON.stringify(upgrades))
+    const saveData = encodeURIComponent(JSON.stringify(player))
+    localStorage.setItem("game-data_player", btoa(saveData))
+}
+
+function fixSave() {
+	defaultData = getRawData();
+	fixData(defaultData, player);
+}
+
+function fixData(defaultData, newData) {
+	for (item in defaultData) {
+		if (defaultData[item] == null) {
+			if (newData[item] === undefined)
+				newData[item] = null;
+		}
+		else if (Array.isArray(defaultData[item])) {
+			if (newData[item] === undefined)
+				newData[item] = defaultData[item];
+
+			else
+				fixData(defaultData[item], newData[item]);
+		}
+		else if (defaultData[item] instanceof Decimal) { // Convert to Decimal
+			if (newData[item] === undefined)
+				newData[item] = defaultData[item];
+
+			else
+				newData[item] = new Decimal(newData[item]);
+		}
+		else if ((!!defaultData[item]) && (typeof defaultData[item] === "object")) {
+			if (newData[item] === undefined || (typeof defaultData[item] !== "object"))
+				newData[item] = defaultData[item];
+
+			else
+				fixData(defaultData[item], newData[item]);
+		}
+		else {
+			if (newData[item] === undefined)
+				newData[item] = defaultData[item];
+		}
+	}
 }
 
 function load() {
-    const data_player = JSON.parse(localStorage.getItem("game-data_player"))
-    const data_upgrades = JSON.parse(localStorage.getItem("game-data_upgrades"))
-
-    if (data_player) {
-        player = convertObjectValuesToEtern(data_player)
+    const data = JSON.parse(decodeURIComponent(atob(localStorage.getItem("game-data_player"))))
+    
+    if (!data) {
+        player = getRawData()
+        return;
     }
 
-    if (data_upgrades) {
-        upgrades = convertObjectValuesToEtern(data_upgrades)
+    player = Object.assign(getRawData(), data)
+    fixSave()
+    document.getElementById("pre-load-hardReset").style.display = "none"
+
+    if (player.didHardReset) {
+        hardReset2() //for true reseting
     }
+}
+
+async function copyToClipboard(text) {
+  try {
+    await navigator.clipboard.writeText(text);
+    console.log('Text copied to clipboard');
+  } catch (err) {
+    console.error('Failed to copy text: ', err);
+  }
+}
+
+
+function exportData() {
+    copyToClipboard(btoa(encodeURIComponent(JSON.stringify(player))))
+    alert("Copied data to clipboard")
+}
+
+function importData() {
+	let imported = prompt("Paste your save here");
+	try {
+		tempPlr = Object.assign(getRawData(), JSON.parse(decodeURIComponent(atob(imported))));
+		player = tempPlr;
+		fixSave();
+		save();
+		window.location.reload();
+	} catch (e) {
+		return;
+	}
 }
 
 function getBase(path, keyword) {
     let base = E(1)
-    
     if (keyword === "add") base = E(0)
-
     for (const key in path) {
         if (key.startsWith(keyword)) {
             const val = path[key]
@@ -111,7 +286,6 @@ function getBase(path, keyword) {
             base = base.mul(val)
         }
     }
-
     return base
 }
 
@@ -121,13 +295,27 @@ function getMulti(path) {
     effect = effect.pow(getBase(path, "pow"))
     effect = effect.div(getBase(path, "div"))
     effect = effect.root(getBase(path, "root"))
-    
     return effect
 }
 
 function hardReset() {
-    player = clearPlayer
-    upgrades = clearUpgrades
-    save()
-    location.reload()
+    player = getRawData();
+    player.didHardReset = true
+    save();
+    location.reload();
+}
+
+function hardReset2() {
+    player = getRawData();
+    save();
+    location.reload();
+}
+
+function hROption() {
+    let conf = prompt("Are you sure you want to fully wipe off all your data? This action cannot be undone\n Type: 'yes yes i want to reset' to confirm.")
+
+    if (conf.toLowerCase() === "yes yes i want to reset") {
+        let conf2 = confirm("This is last warning. Click OK if you are sure")
+        if (conf2) hardReset()
+    }
 }
--- main.js
+++ main.js
@@ -1,61 +1,172 @@
 let lastUpdate = Date.now()
 const pointGrowthFactor = 33/1000
 
+displayPrestigeGainText = "Reach 1e40 Points first to reset"
+
 softcapLevelLabel = [
     Format(getMulti(multi.points.softcap_start['1'])),
-    Format('1e40'),
+    Format(getMulti(multi.points.softcap_start['2'])),
     `${Format('1.79e308')} fr`,
+    Format('1e1000'),
     "inf"
 ]
 
 softcapLevelFormulas = [
-    function() {multi.points.div_softcap1 = player.points.div(getMulti(multi.points.softcap_start['1'])).root(8)},
-    function() {multi.points.div_softcap2 = player.points.log10().sub(39).pow(1.5)},
-    function() {multi.points.root_softcap3 = player.points.log10().sub(308.25).root(3)},
+    function() {multi.points.div_softcap1 = player.points.amount.div(getMulti(multi.points.softcap_start['1'])).root(8)},
+    function() {multi.points.div_softcap2 = player.points.amount.log10().sub(getMulti(multi.points.softcap_start['2']).log10().sub(1)).pow(2)},
+    function() {multi.points.div_softcap3 = player.points.amount.add(1).log10().sub(308.25).pow(100)},
+    function() {multi.points.div_softcap4 = player.points.amount.log(1.01).pow(2)},
     function() {return},
 ]
 
-function update(){
-    if (player.points.gte('1.79e308')) {
-        player.softcapLevel = 3
-    } else if (player.points.gte('1e40')) {
-        player.softcapLevel = 2
-    } else if (player.points.gte(getMulti(multi.points.softcap_start['1']))) {
-        player.softcapLevel = 1
+function ResetUpgradesUnderPath(path) {
+    for (const upgId in path) {
+        let upgrade = path[upgId]
+        upgrade.level = E(0)
+        upgrade.cost = upgrade.baseCost
+    }
+}
+
+function PrestigeReset() {
+    player.points.amount = E(1)
+    player.points.best = E(1)
+    player.points.levelXp = E(0)
+    player.points.level = E(0)
+    ResetUpgradesUnderPath(player.upgrades.points)
+}
+
+function prestige() {
+    if (!player.points.amount.gte(1e40)) return
+    if (!player.unlocks.prestige) return
+    let doReset = false
+
+    if (player.options.confirms.prestige) {
+        doReset = confirm("Are you sure you want to do Prestige?")
     } else {
-        player.softcapLevel = 0
+        doReset = true
     }
 
-    for (i = 0; i < player.softcapLevel; i++) {
+    if (!doReset) return
+
+    player.prestige.prestiged = true
+    player.prestige.points = player.prestige.points.add(nonSavePlayer.prestigePointGain)
+    player.prestige.total = player.prestige.total.add(nonSavePlayer.prestigePointGain)
+    player.prestige.best = player.prestige.points.gt(player.prestige.best) ? player.prestige.points : player.prestige.best
+
+    player.prestige.levelXp = player.prestige.prestiged ? player.prestige.levelXp.add(getMulti(multi.prestige.xp)) : E(0)
+
+    PrestigeReset()
+}
+
+function openTab(id, type) {
+    let tabs = document.querySelectorAll(".tab-"+type);
+    
+    tabs.forEach(function(tab) {
+        tab.style.display = "none";
+    });
+
+    document.getElementById(id + "-tab").style.display = "block";
+}
+
+function update(){
+    //every tick (0.033s)
+    if (player.points.amount.gte('1e1000')) {
+        nonSavePlayer.softcapLevel = 4
+    } else if (player.points.amount.gte('1.79e308')) {
+        nonSavePlayer.softcapLevel = 3
+    } else if (player.points.amount.gte(getMulti(multi.points.softcap_start['2']))) {
+        nonSavePlayer.softcapLevel = 2
+    } else if (player.points.amount.gte(getMulti(multi.points.softcap_start['1']))) {
+        nonSavePlayer.softcapLevel = 1
+    } else {
+        nonSavePlayer.softcapLevel = 0
+    }
+
+    for (i = 0; i < nonSavePlayer.softcapLevel; i++) {
         softcapLevelFormulas[i]()
     }
 
-    multi.points.mul_upg_p1 = E(1.4).pow(upgrades.points['1'].level)
-    multi.points.softcap_start['1'].mul_upg_p2 = E(5).pow(upgrades.points['2'].level)
+    let normalPointXPReq = E(2).pow(player.points.level).mul(10)
+    let normalPrestigeXPReq = E(2).pow(player.prestige.level).mul(10)
+
+    player.points.level = player.points.levelXp.div(10).add(1).log(2).floor()
+    player.prestige.level = player.prestige.levelXp.div(10).add(1).log(2).floor()
+
+    nonSavePlayer.prestigePointGain = player.points.amount.gte(1e40) ? player.points.amount.div(1e40).log(1.12).mul(getMulti(multi.prestige)) : E(0)
+    nonSavePlayer.pointXpReq = player.points.level.eq(0) ? E(10) : normalPointXPReq
+    nonSavePlayer.pointXpCur = player.points.level.eq(0) ? player.points.levelXp : player.points.levelXp.add(10).sub(normalPointXPReq)
+    nonSavePlayer.prestigeXpReq = player.prestige.level.eq(0) ? E(10) : normalPrestigeXPReq
+    nonSavePlayer.prestigeXpCur = player.prestige.level.eq(0) ? player.prestige.levelXp : player.prestige.levelXp.add(10).sub(normalPrestigeXPReq)
+
+    displayPrestigeGainText = player.points.amount.gte(1e40) ? `+${Format(nonSavePlayer.prestigePointGain)} Prestige Points` : "Reach 1e40 Points first to reset"
+
+    multi.points.xp.add_base = player.prestige.prestiged ? player.points.amount.add(1).log(100) : E(0)
+    multi.prestige.xp.add_base = player.prestige.points.add(1).log(1.3)
+
+    multi.points.mul_prestigeBonus = player.prestige.points.add(1).pow(0.7)
+    multi.points.mul_pointLevelBonus = E(1.05).pow(player.points.level)
+    multi.points.xp.mul_prestigeLevelBonus = E(1.5).pow(player.prestige.level)
+    multi.points.softcap_start['1'].mul_pointLevelBonus = E(1.2).pow(player.points.level)
+    multi.points.softcap_start['2'].mul_pointLevelBonus = E(1.5).pow(player.points.level)
+    multi.prestige.mul_prestigeLevelBonus = E(1.04).pow(player.prestige.level)
+
+    multi.points.mul_upg_p1 = (E(1.4).add(player.prestige.level.div(100))).pow(player.upgrades.points['1'].level)
+    multi.points.softcap_start['1'].mul_upg_p2 = E(5).pow(player.upgrades.points['2'].level)
+    multi.points.xp.mul_upg_p3 = E(1.3).pow(player.upgrades.points['3'].level)
+    multi.points.mul_upg_pr1 = (E(1.75).add(player.prestige.level.div(100))).pow(player.upgrades.prestige['1'].level)
+    multi.points.xp.mul_upg_pr2 = E(2).pow(player.upgrades.prestige['2'].level)
+
+    document.getElementById("point-level-bar").style.width = ((nonSavePlayer.pointXpCur.div(nonSavePlayer.pointXpReq).mul(100).toNumber())-1)+"%"
+    document.getElementById("prestige-level-bar").style.width = ((nonSavePlayer.prestigeXpCur.div(nonSavePlayer.prestigeXpReq).mul(100).toNumber())-1)+"%"
 }
 
 function update2(){
-    if (player.softcapLevel === 3) {
-
-    } else if (player.softcapLevel === 2) {
-        multi.points.root_softcap3 = E(1)
-    } else if (player.softcapLevel === 1) {
-        multi.points.root_softcap3 = E(1)
+    //every 0.2s
+    if (nonSavePlayer.softcapLevel === 3) {
+        multi.points.root_softcap4 = E(1)
+    } else if (nonSavePlayer.softcapLevel === 2) {
+        multi.points.root_softcap4 = E(1)
+        multi.points.div_softcap3 = E(1)
+    } else if (nonSavePlayer.softcapLevel === 1) {
+        multi.points.root_softcap4 = E(1)
+        multi.points.div_softcap3 = E(1)
         multi.points.div_softcap2 = E(1)
     } else {
-        multi.points.root_softcap3 = E(1)
+        multi.points.root_softcap4 = E(1)
+        multi.points.div_softcap3 = E(1)
         multi.points.div_softcap2 = E(1)
         multi.points.div_softcap1 = E(1)
     }
 
     softcapLevelLabel[0] = Format(getMulti(multi.points.softcap_start['1']))
+    softcapLevelLabel[1] = Format(getMulti(multi.points.softcap_start['2']))
 
-    for (const layer in upgrades) {
-        for (const upgradeId in upgrades[layer]) {
+    player.prestige.best = player.prestige.points.gt(player.prestige.best) ? player.prestige.points : player.prestige.best
+
+    if (!player.unlocks.prestige && nonSavePlayer.softcapLevel >= 2) {
+        player.unlocks.prestige = true
+    }
+
+    let prest = document.querySelectorAll(".prestige")
+    prest.forEach(function(tab) {
+        if (tab.id === "locked") {
+            tab.style.display = player.unlocks.prestige ? "none" : "block";
+        } else if (tab.id === "unlocked") {
+            tab.style.display = player.unlocks.prestige ? "block" : "none";
+        }
+    });
+
+    document.querySelector(".prestige-levels").style.display = player.prestige.prestiged ? "inline-block" : "none"
+    document.querySelector("#points-3").style.display = player.prestige.prestiged ? "inline-block" : "none"
+    document.querySelector("#prestige-2").style.display = player.prestige.prestiged ? "inline-block" : "none"
+
+    for (const layer in player.upgrades) {
+        for (const upgradeId in player.upgrades[layer]) {
             const id = layer+"-"+upgradeId
             const upgradeElement = document.getElementById(id)
-            const upgrade = upgrades[layer][upgradeId]
-            const currency = player[layer]
+            const upgrade = player.upgrades[layer][upgradeId]
+            const curPath = currencyStringValues[upgrade.currency]
+            const currency = unpackStringToStat(curPath)
             
             if (upgrade.level.gte(upgrade.levelCap)) {
                 upgradeElement.className = "maxed"
@@ -70,25 +181,30 @@ function update2(){
     }
 }
 
-/*function unpackStringToStat(str) {
+function unpackStringToStat(str) {
     let path = player
     let spl = str.split(".")
 
     for (let i = 0; i < spl.length; i++) {
-        path = path[spl]
+        path = path[spl[i]]
     }
 
     return path
-}*/
+}
+
+function setStringToStatValueTo(str, val) {
+    eval(`player.${str} = E(${val})`)
+}
 
 function buyUpgrade(type, id) {
-    const upgrade = upgrades[type][id]
+    const upgrade = player.upgrades[type][id]
+    const curPath = currencyStringValues[upgrade.currency]
     const cost = upgrade.cost
-    //let currency = unpackStringToStat(upgrade.currency)
+    const currency = unpackStringToStat(curPath)
     const doCost = upgrade.doCost
     const upgradeForm = upgradeFormulas[type][id]
 
-    if (player[type].lt(cost)) return
+    if (currency.lt(cost)) return
     if (upgrade.level.gte(upgrade.levelCap)) return
     console.log(type, id)
 
@@ -98,20 +214,22 @@ function buyUpgrade(type, id) {
     upgrade.cost = upgradeForm.costForm()
 
     if (doCost === 2) {
-        player[type] = upgrade.resetTo
+        setStringToStatValueTo(curPath, upgrade.resetTo)
     } else if (doCost === 1) {
-        player[type] = player[type].sub(upgradeForm.costForm(1))
+        setStringToStatValueTo(curPath, unpackStringToStat(curPath).sub(upgradeForm.costForm(1)))
     }
 }
 
 function gain(diff) {
     // Points
-    let base = getMulti(multi.points).pow(diff)
-    player.points = player.points.mul(base)
+    player.points.amount = player.points.amount.mul(getMulti(multi.points).pow(pointGrowthFactor))
+    player.points.levelXp = player.prestige.prestiged ? player.points.levelXp.add(getMulti(multi.points.xp).mul(diff)) : E(0)
+
+    player.points.best = player.points.amount.gt(player.points.best) ? player.points.amount : player.points.best
 }
 
 function tick() {
-    let diff = (Date.now()-lastUpdate)/1000
+    let diff = Math.min((Date.now()-lastUpdate)/1000, 1)
     gain(diff)
     update()
     lastUpdate = Date.now()
@@ -119,7 +237,9 @@ function tick() {
 
 load()
 
-setInterval(tick, 33)
-setInterval(update2, 200)
+setTimeout(function() {
+    setInterval(tick, 33)
+    setInterval(update2, 200)
 
-setInterval(save, 3000)
+    setInterval(save, 3000)
+}, 500)
